4f73d072fa1dd2f801322e0098da8e1c45706f28
Gbubemi Attah <attahgbube@gmail.com>
2025-01-19 15:46:12 +0100

    added vercel json file

--- src/pages/beam-calculator/CalculatorLayout.jsx
+++ src/pages/beam-calculator/CalculatorLayout.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Sidebar from "./Sidebar";
 import Output from "./Output";
+import AlertModal from "./AlertModal";
 
 export default function CalculatorLayout() {
   return (
     <div className="sm:flex sm:flex-row w-full min-h-screen h-screen max-h-screen overflow-hidden">
+      {/* <AlertModal/> */}
       <Sidebar />
       <Output />
     </div>
